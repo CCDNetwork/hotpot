895fded7422ed57189182223c28a6cd225d811e0
Luka Radenovic <luka.radenovic2@gmail.com>
2026-01-13 15:34:53 +0100

    feat: add download templates functionality to Booking and ViewBooking pages

--- client/src/modules/BookingPage/BookingPage.tsx
+++ client/src/modules/BookingPage/BookingPage.tsx
@@ -1,10 +1,21 @@
-import { Wand2Icon } from 'lucide-react';
+import {
+  DownloadIcon,
+  FileDownIcon,
+  FileTextIcon,
+  Wand2Icon,
+} from 'lucide-react';
 
 import { PageContainer } from '@/components/PageContainer';
 import { Button } from '@/components/ui/button';
 import { APP_ROUTE } from '@/helpers/constants';
 import { useState } from 'react';
 import { BookingWizard } from './components';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export const BookingPage = () => {
   const [isBookingWizardOpen, setIsBookingWizardOpen] =
@@ -18,6 +29,38 @@ export const BookingPage = () => {
     <PageContainer
       pageTitle="Booking Cases"
       pageSubtitle="On this page you can run a CWG booking wizard. To run CWG booking, click on the Booking Wizard button below."
+      headerNode={
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button type="button">
+              <DownloadIcon className="mr-2 w-4 h-4" />
+              Download template
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="p-0">
+              <a
+                href="/booking-empty-upload-template.xlsx"
+                download
+                className="flex gap-1 items-center px-2 py-1"
+              >
+                <FileDownIcon className="w-4 h-4" />
+                Empty template
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="p-0">
+              <a
+                href="/booking-template-with-readme.xlsx"
+                download
+                className="flex gap-1 items-center px-2 py-1"
+              >
+                <FileTextIcon className="w-4 h-4" />
+                Template with readme
+              </a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      }
       breadcrumbs={[
         {
           href: `${APP_ROUTE.Booking}`,
--- client/src/modules/ViewBookingPage/ViewBookingPage.tsx
+++ client/src/modules/ViewBookingPage/ViewBookingPage.tsx
@@ -8,6 +8,14 @@ import { columns } from './columns';
 import { useState } from 'react';
 import { DateRangePickerFilter } from '@/components/DataTable/DateRangePickerFilter';
 import { FilterDropdown } from '@/components/DataTable/FilterDropdown';
+import { Button } from '@/components/ui/button';
+import { DownloadIcon, FileDownIcon, FileTextIcon } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export const ViewBookingPage = () => {
   const pagination = usePagination();
@@ -34,7 +42,38 @@ export const ViewBookingPage = () => {
     <PageContainer
       pageTitle="View Bookings"
       pageSubtitle="On this page you can view all your bookings."
-      headerNode={null}
+      headerNode={
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button type="button">
+              <DownloadIcon className="mr-2 w-4 h-4" />
+              Download template
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="p-0">
+              <a
+                href="/booking-empty-upload-template.xlsx"
+                download
+                className="flex gap-1 items-center px-2 py-1"
+              >
+                <FileDownIcon className="w-4 h-4" />
+                Empty template
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="p-0">
+              <a
+                href="/booking-template-with-readme.xlsx"
+                download
+                className="flex gap-1 items-center px-2 py-1"
+              >
+                <FileTextIcon className="w-4 h-4" />
+                Template with readme
+              </a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      }
       breadcrumbs={[
         {
           href: `${APP_ROUTE.ViewBooking}`,
